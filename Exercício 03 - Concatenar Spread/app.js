c868111d85a5495c8055cfa9212c6b52bcfd26ee
Murillo Silva <murillohpsilva@gmail.com>
2022-10-18 21:47:06 -0300

    Commit #3 - Exercício 03 - Concatenar Spread
    Resolução do terceiro exercício da lista

---
+++ Exercício 03 - Concatenar Spread/app.js
@@ -0,0 +1,8 @@
+const arrayx = [1, 2, 3];
+const arrayy = [4, 5, 6];
+
+const concatena = (array1, array2) => {return array1.concat(array2);};
+
+const arrayz = concatena(arrayx, arrayy);
+
+console.log(arrayz);
